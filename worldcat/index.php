eefd334c9c066dcd6b9e98960a00fc9bc3951344
Mike Curtis <curtismi@upstate.edu>
2012-07-26 17:18:38 -0400

    added marcxml2php object to worldcat view

--- worldcat/index.php
+++ worldcat/index.php
@@ -5,6 +5,7 @@ require_once(dirname(__FILE__).'/../classes/Config.php');
 require_once(dirname(__FILE__).'/../classes/Input.php');
 require_once(dirname(__FILE__).'/../classes/Pagination.php');
 require_once(dirname(__FILE__).'/../classes/SruClient.php');
+require_once(dirname(__FILE__).'/../classes/MarcXml2Php.php');
 require_once(dirname(__FILE__).'/../private.php');
 $cfg_path = dirname(__FILE__).'/worldcat_sru.yaml';
 // create config instance and set client arguments
@@ -17,6 +18,7 @@ $schema = $cfg->getValue('recordSchema');
 $i = new Input($_GET);
 $p = new Pagination($cfg->getValue('limit')); // or maximumRecords
 $sru = new SruClient($baseurl,$max,$packing,$schema);
+$marc = new MarcXml2Php(NULL);
 // set search arguments
 $query = $i->getValue('query');
 $start = $i->getValue('start');
@@ -27,6 +29,7 @@ if($query !== ''){
   $hits = $sru->response['numberOfRecords'];
   $p->setValues($hits,$start);
   $p->setURLs($_GET);
+  $results = $marc->parse($xml);
 }
 // display
 include_once('template.html');
