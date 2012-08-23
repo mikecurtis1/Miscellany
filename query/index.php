f5cf728dc908ddd52facb46133cb08ce485a2739
Mike Curtis <curtismi@upstate.edu>
2012-08-23 10:13:32 -0400

    added ini set error display

--- query/index.php
+++ query/index.php
@@ -1,5 +1,6 @@
 <?php 
 error_reporting(E_ALL);
+ini_set('display_errors', '1');
 require_once(dirname(__FILE__).'/../classes/QueryParser.php');
 $q = new QueryParser();
 $tokens = $q->parseQuery($_GET['q']);
