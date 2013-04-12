a3633ed3de356d5ecb8c74e1b79b9fb45b8632c3
Mike Curtis <curtismi@upstate.edu>
2013-04-12 15:50:57 -0300

    Create SimpleXml.php

---
+++ classes/SimpleXml.php
@@ -0,0 +1,63 @@
+<?php 
+class SimpleXml 
+{
+  private $_parser;
+	
+	public function __construct($xml=NULL,$class_name=NULL,$options=NULL,$ns=NULL,$is_prefix=NULL){
+		try {
+			$this->_parser = new SimpleXMLElement($xml,$class_name,$options,$ns,$is_prefix);
+		} catch (Exception $e) {
+			return 'Caught exception: ' . $e->getMessage();
+		}
+	}
+	
+	public function regNameSpaces($namespaces=array()){
+		foreach ( $namespaces as $prefix => $uri ) {
+			$this->_parser->registerXPathNamespace($prefix, $uri); //NOTE: determine namespace prefixes and URIs using Firebug
+		}
+
+		return;
+	}
+	
+	public function getXpathXmlDocs($xpath=''){
+		$docs = array();
+		foreach ( $this->_parser->xpath($xpath) as $obj ) { //NOTE: xpath method returns an array or false
+			$docs[] = $obj->asXML();
+		}
+		
+		return $docs;
+	}
+	
+	public function getXpathText($xpath=''){
+		$docs = $this->getXpathXmlDocs($xpath);
+		if ( isset($docs[0]) ) {
+			return $this->_normalizeWhiteSpace(strip_tags($docs[0]));
+		} else {
+			return '';
+		}
+	}
+	
+	public function getXpathAttr($xpath='',$attr_name=''){
+		$a = $this->_parser->xpath($xpath);
+		if ( isset($a[0]->attributes()->{$attr_name}) ) {
+			return $a[0]->attributes()->{$attr_name};
+		} else {
+			return '';
+		}
+	}
+	
+	public function getPlainText($arg=NULL){
+		if ( is_array($arg) ) {
+			return $this->_normalizeWhiteSpace(strip_tags(array_shift($arg)));
+		} elseif ( is_string($arg) ) {
+			return $this->_normalizeWhiteSpace(strip_tags($arg));
+		} else {
+			return '';
+		}
+	}
+
+	private function _normalizeWhiteSpace($string=''){
+		return preg_replace('/\s{2,}/', ' ', trim($string));
+	}
+}
+?>
