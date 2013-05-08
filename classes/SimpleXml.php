3b9c891a7e8708c3750a30eccef4ae8103711f02
Mike Curtis <curtismi@upstate.edu>
2013-05-08 11:01:46 -0300

    Update SimpleXml.php

--- classes/SimpleXml.php
+++ classes/SimpleXml.php
@@ -1,9 +1,10 @@
 <?php 
+
 class SimpleXml 
 {
   private $_parser;
 	
-	public function __construct($xml=NULL,$class_name=NULL,$options=NULL,$ns=NULL,$is_prefix=NULL){
+	public function __construct($xml='',$class_name=NULL,$options=NULL,$ns=NULL,$is_prefix=NULL){
 		try {
 			$this->_parser = new SimpleXMLElement($xml,$class_name,$options,$ns,$is_prefix);
 		} catch (Exception $e) {
@@ -21,8 +22,12 @@ class SimpleXml
 	
 	public function getXpathXmlDocs($xpath=''){
 		$docs = array();
-		foreach ( $this->_parser->xpath($xpath) as $obj ) { //NOTE: xpath method returns an array or false
-			$docs[] = $obj->asXML();
+		if ( $this->_isSimpleXMLElement($this->_parser) === TRUE ) {
+			if ( is_array($this->_parser->xpath($xpath)) ) {
+				foreach ( $this->_parser->xpath($xpath) as $obj ) { //NOTE: xpath method returns an array or false
+					$docs[] = $obj->asXML();
+				}
+			}
 		}
 		
 		return $docs;
@@ -58,6 +63,14 @@ class SimpleXml
 
 	private function _normalizeWhiteSpace($string=''){
 		return preg_replace('/\s{2,}/', ' ', trim($string));
+	}
+	
+	private function _isSimpleXMLElement($object=NULL){
+		if ( is_object($object) && get_class($object) === 'SimpleXMLElement' ) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
 	}
 }
 ?>
