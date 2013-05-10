3851db2ff884c2012009dd3ff666125c45348477
Mike Curtis <curtismi@upstate.edu>
2013-05-10 20:00:52 -0300

    Update index.php

--- z3950/index.php
+++ z3950/index.php
@@ -1,57 +1,80 @@
 <?php 
 /** 
- * http://search.idsproject.org/curtismi/index.php?oclc_symbol=VYQ&view=z3950&host=ups.sunyconnect.suny.edu&port=6363&database=UPS01PUB&username=&password=&syntax=opac&search_type=rpn&rec_type=xml%3B+charset%3Dmarc-8%2Cutf-8&rpn=%40attr+1%3D4+health%20%3E%3E%20search.idssearch.idsproject.org/curtismi/index.php?oclc_symbol=VYQ&view=z3950&host=ups.sunyconnect.suny.edu&port=6363&database=UPS01PUB&username=&password=&syntax=opac&search_type=rpn&rec_type=xml;+charset=marc-8,utf-8&rpn=@attr+1=1007+000087884
+ * index.php?oclc_symbol=VYQ&view=z3950&host=ups.sunyconnect.suny.edu&port=6363&database=UPS01PUB&username=&password=&syntax=opac&search_type=rpn&rec_type=xml%3B+charset%3Dmarc-8%2Cutf-8&rpn=%40attr+1%3D4+health%20%3E%3E%20search.idssearch.idsproject.org/curtismi/index.php?oclc_symbol=VYQ&view=z3950&host=ups.sunyconnect.suny.edu&port=6363&database=UPS01PUB&username=&password=&syntax=opac&search_type=rpn&rec_type=xml;+charset=marc-8,utf-8&rpn=@attr+1=1007+000087884
  */
 // PHP environment
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+header('Content-Type: text/html; charset=utf-8');
+require_once(dirname(__FILE__).'/Request.php');
+require_once(dirname(__FILE__).'/Z3950Client.php');
+require_once(dirname(__FILE__).'/SimpleXml.php');
+require_once(dirname(__FILE__).'/Record.php');
+require_once(dirname(__FILE__).'/Item.php');
+require_once(dirname(__FILE__).'/Isbn.php');
 // config
-$callback = Get::param('callback');
-$oclc_symbol = Get::param('oclc_symbol');
-$host = Get::param('host');
-$port = Get::param('port');
-$database = Get::param('database');
-$username = Get::param('username');
-$password = Get::param('password');
-$syntax = Get::param('syntax');
-$search_type = Get::param('search_type');
-$rec_type = Get::param('rec_type');
-$rpn = Get::param('rpn');
+$callback = Request::value('callback');
+$host = Request::value('host');
+$port = Request::value('port');
+$database = Request::value('database');
+$username = Request::value('username');
+$password = Request::value('password');
+$syntax = Request::value('syntax');
+$search_type = Request::value('search_type');
+$rec_type = Request::value('rec_type');
+$rpn = Request::value('rpn');
+$delimiter = '::'; //chr(31)
 // create client object
 $z = new Z3950Client($host,$port,$database,$username,$password,$syntax,$search_type,$rec_type);
 // run search and get a record set
 $hits = $z->zSearch($rpn);
-$limit = 10;
+$max = 10;
 $end = 1;
-if ( $hits < $limit ) {
+$start = 1;
+if ( $hits < $max ) {
   $end = $hits;
 } else {
-	$end = $limit;
+	$end = $max;
 }
-$recs = $z->getRecords(1,$end);
-// set hits
-$arr = array();
-$arr['hits'] = number_format($hits);
+$recs = $z->getRecords($start,$end);
 // process records
 foreach ( $recs as $n => $rec ) {
+	#$x = new SimpleXml($rec,NULL,NULL,NULL,FALSE);
+	$r = new Record();
+	$i = new Item();
 	$x = new SimpleXml($rec,NULL,NULL,'http://www.loc.gov/MARC21/slim',FALSE);
 	$ns = array('slim'=>'http://www.loc.gov/MARC21/slim');
 	$x->regNameSpaces($ns);
 	$holdings = $x->getXpathXmlDocs('//holding');
-	$arr['recs'][$n]['metadata_source'] = 'Aleph';
-	$arr['recs'][$n]['metadata_source_id'] = $x->getXpathText('//slim:record/slim:controlfield[@tag="001"]');
-	$arr['recs'][$n]['metadata_source_uri'] = 'z3950//:'.$host.':'.$port.'/'.$database.'?query=('.urlencode($rpn).')'; // http://www.gils.net/z-url.txt
-	$arr['recs'][$n]['title'] = $x->getXpathText('//slim:record/slim:datafield[@tag="245"]');
-	$arr['recs'][$n]['authors'][] = $x->getXpathText('//slim:record/slim:datafield[@tag="100"]/slim:subfield[@code="a"]'); //TODO: gather up author data from all associated fields
-	$arr['recs'][$n]['language'] = $x->getXpathText('//slim:record/slim:datafield[@tag="041"]');
-	$arr['recs'][$n]['series'] = $x->getXpathText('//slim:record/slim:datafield[@tag="490"]');
-	$arr['recs'][$n]['description'] = $x->getXpathText('//slim:record/slim:datafield[@tag="520"]/slim:subfield[@code="a"]');
-	if ( $arr['recs'][$n]['description'] === '' ) {
-		$arr['recs'][$n]['description'] = $x->getXpathText('//slim:record/slim:datafield[@tag="505"]');
+	$r->setMetadataSource('Aleph');
+	$r->setMetadataSourceId($x->getXpathText('//slim:record/slim:controlfield[@tag="001"]'));
+	$r->setMetadataSourceUri('z3950//:'.$host.':'.$port.'/'.$database.'?query=('.urlencode($rpn).')'); // http://www.gils.net/z-url.txt
+
+	/*
+	$marc_020 = $x->getXpathXmlDocs('//oai_marc/varfield[@id="020"]/subfield[@label="a"]');
+	$isbn = Isbn::all(implode(',',$marc_020));
+	print_r($isbn);
+	*/
+	$marc_001 = $x->getXpathText('//slim:record/slim:controlfield[@tag="001"]');
+	$marc_020 = $x->getXpathText('//slim:record/slim:datafield[@tag="020"]/subfield[@label="a"]');
+	$isbn = Isbn::first($marc_020);
+	$r = new Record();
+	$r->setTitle($x->getXpathText('//slim:record/slim:datafield[@tag="245"]'));
+	$r->setSeries($x->getXpathText('//slim:record/slim:datafield[@tag="490"]'));
+	$r->setAuthor($x->getXpathText('//slim:record/slim:datafield[@tag="100"]/subfield[@label="a"]'));
+	$r->setEdition($x->getXpathText('//slim:record/slim:datafield[@tag="250"]'));
+	$r->setLanguage($x->getXpathText('//slim:record/slim:datafield[@tag="041"]'));
+	$description = '';
+	if ( $description === '' ) {
+		$description = $x->getXpathText('//slim:record/slim:datafield[@tag="520"]/subfield[@label="a"]');
 	}
-	$arr['recs'][$n]['topics'][] = $x->getXpathText('//slim:record/slim:datafield[@tag="650"]');
+	if ( $description === '' ) {
+		$description = $x->getXpathText('//slim:record/slim:datafield[@tag="505"]/subfield[@label="a"]');
+	}
+	$r->setDescription($description);
 	foreach ( $holdings as $i => $holding ) {
 		$h = new SimpleXml($holding,NULL,NULL,NULL,FALSE);
+		$i = new Item();
 		$temp['localLocation'] = $h->getXpathText('localLocation');
 		$temp['shelvingLocation'] = $h->getXpathText('shelvingLocation');
 		$temp['callNumber'] = $h->getXpathText('callNumber');
@@ -62,34 +85,52 @@ foreach ( $recs as $n => $rec ) {
 		$temp['availabiltyDate'] = $h->getXpathText('circulations/circulation/availabiltyDate');
 		$temp['availableThru'] = $h->getXpathText('circulations/circulation/availableThru');
 		$temp['itemId'] = $h->getXpathText('circulations/circulation/itemId');
-		if ( $temp['copyNumber'] !== '' ) {
-			$arr['recs'][$n]['items'][$i]['identifiers'][] = 'COPY'.chr(31).$temp['copyNumber'];
-		}
-		if ( $temp['itemId'] !== '' ) {
-			$arr['recs'][$n]['items'][$i]['identifiers'][] = 'ITEMID'.chr(31).$temp['itemId'];
-		}
-		if ( $temp['enumAndChron'] !== '' ) {
-			$arr['recs'][$n]['items'][$i]['identifiers'][] = 'ENUMANDCHRON'.chr(31).$temp['enumAndChron'];
-		}
 		$a['publicNote'] = $temp['publicNote'];
 		$a['availableNow'] = $temp['availableNow'];
 		$a['availabiltyDate'] = $temp['availabiltyDate'];
 		$a['availableThru'] = $temp['availableThru'];
-		$arr['recs'][$n]['items'][$i]['access'] = http_build_query($a, '', '&');
-		$arr['recs'][$n]['items'][$i]['copyright'] = '';
-		$arr['recs'][$n]['items'][$i]['location'] = buildLocation($temp);
-		$arr['recs'][$n]['items'][$i]['uri'] = '';
-		$arr['recs'][$n]['items'][$i]['mime_type'] = '';
-		$arr['recs'][$n]['items'][$i]['identifiers'][] = 'ISBN'.chr(31).$x->getXpathText('//slim:record/slim:datafield[@tag="020"]/subfield[@label="a"]');
-		$arr['recs'][$n]['items'][$i]['publication_date'] = $x->getXpathText('//slim:record/slim:datafield[@tag="260"]/slim:subfield[@code="c"]');
-		$arr['recs'][$n]['items'][$i]['publisher'] = $x->getXpathText('//slim:record/slim:datafield[@tag="260"]/slim:subfield[@code="b"]');
-		$arr['recs'][$n]['items'][$i]['physical_description'] = $x->getXpathText('//slim:record/slim:datafield[@tag="300"]');
-		$arr['recs'][$n]['items'][$i]['media_type'] = ''; //TODO: hunt down data in the marc record
-		$arr['recs'][$n]['items'][$i]['edition'] = $x->getXpathText('//slim:record/slim:datafield[@tag="250"]');
+		$i->setMediaType(''); //TODO: build a media type
+		$i->setAccess(http_build_query($a, '', '&'));
+		$i->setCopyright('');
+		$i->setLocation(buildLocation($temp));
+		$i->setUri('');
+		$i->setMimeType('');
+		$i->setPublicationDate($x->getXpathText('//slim:record/slim:datafield[@tag="260"]/slim:subfield[@code="c"]'));
+		$i->setPublisher($x->getXpathText('//slim:record/slim:datafield[@tag="260"]/slim:subfield[@code="b"]'));
+		$i->setPhysicalDescription($x->getXpathText('//slim:record/slim:datafield[@tag="300"]'));
+		$i->setIdentifier('SYS',$delimiter,$marc_001);
+		$i->setIdentifier('ENUMANDCHRON',$delimiter,$temp['enumAndChron']);
+		$i->setIdentifier('ITEMID',$delimiter,$temp['itemId']);
+		$i->setIdentifier('COPY',$delimiter,$temp['copyNumber']);
+		$i->setIdentifier('ISBN',$delimiter,$isbn);
+		$r->setItem($i);
 	}
+	$record_objects[] = $r;
 }
-// display
-$json = json_encode($arr);
+// produce and display JSON
+//TODO: this part is a mess! I should probably have a Records object with a toJson() method
+function recordObjects2Array($obj) {
+	if ( is_object($obj) ) {
+		$obj = (array) $obj;
+	}
+	if ( is_array($obj) ) {
+		$new = array();
+		foreach($obj as $key => $val) {
+			$new[$key] = recordObjects2Array($val);
+		}
+	}
+	else { 
+		$new = $obj;
+	}
+	return $new;
+}
+$arr = array();
+$arr['query'] = $rpn;
+$arr['start'] = $start;
+$arr['max'] = $max;
+$arr['hits'] = $hits;
+$arr['records'] = recordObjects2Array($record_objects);
+$json = str_replace('\\u0000', '', json_encode($arr));
 header('Content-Type: application/json; charset=utf-8'); 
 if ( $callback !== '' ) {
 	echo $callback.'('.$json.');';
@@ -111,6 +152,4 @@ function buildLocation($arr=array(),$delimiter='; '){
 	
 	return $location;
 }
-function buildMediaType(){}
-function buildAuthors(){}
 ?>
