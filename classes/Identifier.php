76135b5f341e329117de82957bd4c215b916a6c9
Mike Curtis <curtismi@upstate.edu>
2013-05-11 12:17:24 -0300

    Create Identifier.php

---
+++ classes/Identifier.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Identifier 
+{
+  public $label;
+	public $delimiter;
+	public $value;
+	
+	static public function build($label=NULL,$delimiter=NULL,$value=NULL){
+		if ( is_string($label) && is_string($delimiter) && is_string($value) ) {
+			$obj = new Identifier;
+			$obj->label = $label;
+			$obj->delimiter = $delimiter;
+			$obj->value = $value;
+			return $obj;
+		} else {
+			return FALSE;
+		}
+	}
+}
+?>
