b37bf089b8ba1072359a53a86a7e6b9bfba02dae
Mike Curtis <curtismi@upstate.edu>
2013-05-24 18:03:42 -0400

    Create ScreenScrapePdfUrl.php

---
+++ classes/ScreenScrapePdfUrl.php
@@ -0,0 +1,62 @@
+<?php 
+
+class ScreenScrapePdfUrl
+{
+	private $_source_url = '';
+	private $_html = '';
+	private $_host = '';
+	private $_url = FALSE;
+	private $_regex = '';
+	private $_regex_ebscohost = "\<a id\=\"downloadLink\".*?href\=\"(.*?)\".*?\>Download PDF\<\/a\>";
+	private $_regex_sciencedirect = "\<a id\=\"pdfLink\".*?href\=\"(.*?)\".*?title\=\"Download PDF\".*?\>PDF.*?\<\/a\>";
+	private $_regex_ovid = "\<iframe.*?\>\<a href\=\"(.*?)\"\>.*?\<\/a\>\<\/iframe\>";
+	
+	static public function build($arg=NULL){
+		if ( self::_isUrl($arg) ) {
+			$obj = new ScreenScrapePdfUrl;
+			$obj->_source_url = $arg;
+			$obj->_html = file_get_contents($arg);
+			$obj->_host = parse_url($arg, PHP_URL_HOST);
+			$obj->_setRegex();
+			$obj->_url = $obj->_screenScrape();
+			return $obj;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	static private function _isUrl($arg=NULL){
+		if ( filter_var($arg, FILTER_VALIDATE_URL) ) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	private function _setRegex(){
+		if ( strpos($this->_host, 'ebscohost.com') !== FALSE ) {
+			$this->_regex = $this->_regex_ebscohost;
+		} elseif ( strpos($this->_host, 'sciencedirect.com') !== FALSE ) {
+			$this->_regex = $this->_regex_sciencedirect;
+		} elseif ( strpos($this->_host, 'ovid.com') !== FALSE ) {
+			$this->_regex = $this->_regex_ovid;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	private function _screenScrape(){
+		if ( $this->_regex !== '' ) {
+			if ( preg_match("/".$this->_regex."/si",$this->_html,$match) === 1 ) {
+				return trim(htmlspecialchars_decode($match[1]));
+			} else {
+				return FALSE;
+			}
+		}
+	}
+	
+	public function getUrl(){
+		return $this->_url;
+	}
+}
+?>
