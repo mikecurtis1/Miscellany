16b876529748ee62a9ae8500d8cfb9de9a41ce04
Mike Curtis <curtismi@upstate.edu>
2013-05-25 19:35:47 -0300

    Update Identifier.php

--- classes/Identifier.php
+++ classes/Identifier.php
@@ -6,17 +6,16 @@ class Identifier
 	private $_delimiter = '';
 	private $_value = '';
 	
-	public function __construct($type=NULL,$delimiter=NULL,$value=NULL){
+	/*public function __construct($type=NULL,$delimiter=NULL,$value=NULL){
 		if ( is_string($type) && is_string($delimiter) && is_string($value) ) {	
 			$this->_type = $type;
 			$this->_delimiter = $delimiter;
 			$this->_value = $value;
 		}
-	}
+	}*/
 	
 	//TODO: decide, either always construct,  but only set variables when conditions met. Or, only construct if conditions are met. I don't know?
 	
-	/*
 	private function __construct($type=NULL,$delimiter=NULL,$value=NULL){
 		$this->_type = $type;
 		$this->_delimiter = $delimiter;
@@ -30,7 +29,6 @@ class Identifier
 			return FALSE;
 	 	}
 	}
-	*/
 	
 	public function __toString(){
 		return $this->_type.$this->_delimiter.$this->_value;
