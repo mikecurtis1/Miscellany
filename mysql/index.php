e852b78dadefc9a3478204a8fa8989fc6673f493
Mike Curtis <curtismi@upstate.edu>
2013-05-31 17:10:55 -0400

    Create index.php

---
+++ mysql/index.php
@@ -0,0 +1,29 @@
+<pre>
+<?php 
+// PHP environment
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+// input
+$input = '';
+if ( isset($_GET['input']) ) {
+	$input = $_GET['input'];
+}
+// includes
+require_once('DbConnection.php');
+require_once('DbQuery.php');
+// run
+$results = array();
+if ( $conn = DbConnection::create('127.0.0.1', 'root', '') ) {
+	/*
+	$_input = mysql_real_escape_string($input,$conn->getConnection());
+	echo var_dump($_input);
+	*/
+	$db = 'reserves';
+	$sql = 'SELECT * FROM FILES';
+	if ( $query = DbQuery::query($conn->getConnection(),$db,$sql) ) {
+		$results = $query->getResults();
+	}
+}
+echo var_dump($results);
+?>
+</pre>
