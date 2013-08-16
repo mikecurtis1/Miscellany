2cd7a56ed2e572d3339f060f56e2ed235095dc03
Mike Curtis <curtismi@upstate.edu>
2013-08-16 17:47:32 -0400

    Update and rename error.html to error.php

---
+++ purl/error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>404 Not Found</title>
+</head>
+<body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+<p>Contact the <a href="mailto:library@example.com">Library</a> for help.</p>
+<hr>
+<?php echo $_SERVER['SERVER_SIGNATURE']; ?>
+</body>
+</html>
