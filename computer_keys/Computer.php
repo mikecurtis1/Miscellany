5cc9cbad8fd342b470f56bfcd4cc85fff7d2c166
Mike Curtis <curtismi@upstate.edu>
2013-08-25 02:12:29 -0400

    Update Computer.php

--- computer_keys/Computer.php
+++ computer_keys/Computer.php
@@ -4,16 +4,18 @@ class Computer
 {
 	private $_name = NULL;
 	private $_ip = NULL;
+	private $_id = NULL;
 	private $_time_blocks = array();
 	
-	private function __construct($name=NULL,$ip=NULL){
+	private function __construct($name=NULL,$id=NULL,$ip=NULL){
 		$this->_name = $name;
+		$this->_id = $id;
 		$this->_ip = $ip;
 	}
 	
-	public static function create($name=NULL,$ip=NULL){
-		if ( is_string($name) && filter_var($ip,FILTER_VALIDATE_IP) ) {
-			return new Computer($name,$ip);
+	public static function create($name=NULL,$id=NULL,$ip=NULL){
+		if ( is_string($name) && is_numeric($id) && filter_var($ip,FILTER_VALIDATE_IP) ) {
+			return new Computer($name,$id,$ip);
 		} else {
 			return FALSE;
 		}
@@ -27,6 +29,10 @@ class Computer
 	
 	public function getName(){
 		return $this->_name;
+	}
+	
+	public function getId(){
+		return $this->_id;
 	}
 	
 	public function getIP(){
