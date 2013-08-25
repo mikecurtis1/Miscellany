b9c350a1c7e3deb1c715b84a8e4570ec72cd7536
Mike Curtis <curtismi@upstate.edu>
2013-08-25 17:05:32 -0400

    Update Computer.php

--- computer_keys/Computer.php
+++ computer_keys/Computer.php
@@ -1,20 +1,24 @@
 <?php 
 
 require_once(dirname(__FILE__).'/Event.php');
+require_once(dirname(__FILE__).'/TimeBlock.php');
 
 class Computer
 {
 	private $_name = NULL;
 	private $_ip = NULL;
 	private $_id = NULL;
+	private $_day_begin = NULL;
+	private $_day_end = NULL;
 	private $_time_blocks = array();
-	private $_scheduled_events = array();
-	private $_available_events = array();
+	private $_available_time_blocks = array();
 	
 	private function __construct($name=NULL,$id=NULL,$ip=NULL){
 		$this->_name = $name;
 		$this->_id = $id;
 		$this->_ip = $ip;
+		$this->_day_begin = (string) strtotime(date("Y-m-d").' 00:00:00');
+		$this->_day_end = (string) strtotime(date("Y-m-d").' 23:59:59');
 	}
 	
 	public static function create($name=NULL,$id=NULL,$ip=NULL){
@@ -28,6 +32,67 @@ class Computer
 	public function addTimeBlock($arg=NULL){
 		if ( $arg instanceof TimeBlock ) {
 			$this->_time_blocks[] = $arg;
+		}
+	}
+
+	//TODO: make this function smaller, break it up?
+	public function setAvailableTimeBlocks(){
+		// init arrays
+		$scheduled_events = array();
+		$inverted_events = array();
+		$previous_event = NULL;
+		// sort first
+		$this->sortTimeBlocks();
+		// list scheduled events
+		foreach ( $this->getTimeBlocks() as $block ) {
+			if ( $e = Event::create('BEGIN',$block->getBegin()) ) {
+				$scheduled_events[] = $e;
+			}
+			if ( $e = Event::create('END',$block->getEnd()) ) {
+				$scheduled_events[] = $e;
+			}
+		}
+		// 'invert' scheduled events
+		foreach ( $this->getTimeBlocks() as $block ) {
+			if ( $e = Event::create('END',$block->getBegin()-1) ) {
+				$inverted_events[] = $e;
+			}
+			if ( $e = Event::create('BEGIN',$block->getEnd()+1) ) {
+				$inverted_events[] = $e;
+			}
+		}
+		// if beginning of day is not scheduled, add it
+		if ( $this->_day_begin !== reset($scheduled_events)->getTime() && reset($scheduled_events)->getType() === 'BEGIN' ) {
+			if ( $e = Event::create('BEGIN',$this->_day_begin) ) {
+				array_unshift($inverted_events,$e);
+			}
+		}
+		// if end of day is not scheduled, add it
+		if ( $this->_day_end !== end($scheduled_events)->getTime() && end($scheduled_events)->getType() === 'END' ) {
+			if ( $e = Event::create('END',$this->_day_end) ) {
+				array_push($inverted_events,$e);
+			}
+		}
+		// read inverted event sequentially, create available time blocks for BEGIN/END pairs
+		foreach ( $inverted_events as $e ) {
+			if ( $previous_event instanceof Event && $previous_event->getType() === 'BEGIN' && $e->getType() === 'END' ) {
+				if ( $available_time_block = TimeBlock::create($previous_event->getTime(),$e->getTime(),'AVAILABLE') ) {
+					$this->_available_time_blocks[] = $available_time_block;
+				}
+			}
+			$previous_event = $e;
+		}
+	}
+	
+	public function sortTimeBlocks(){
+		usort($this->_time_blocks,array('Computer','cmpTimeBlocks'));
+	}
+	
+	public static function cmpTimeBlocks($a,$b){
+		if ( $a instanceof TimeBlock && $b instanceof TimeBlock ) {
+			return ($a->getBegin() < $b->getBegin()) ? -1 : 1;
+		} else {
+			return 1;
 		}
 	}
 	
@@ -45,36 +110,6 @@ class Computer
 	
 	public function getTimeBlocks(){
 		return $this->_time_blocks;
-	}
-	
-	public function getScheduledEvents(){
-		return $this->_scheduled_events;
-	}
-	
-	public function sortTimeBlocks(){
-		usort($this->_time_blocks,array('Computer','cmpTimeBlocks'));
-	}
-	
-	public static function cmpTimeBlocks($a,$b){
-		if ( $a instanceof TimeBlock && $b instanceof TimeBlock ) {
-			return ($a->getBegin() < $b->getBegin()) ? -1 : 1;
-		} else {
-			return 1;
-		}
-	}
-	
-	public function setScheduledEvents(){
-		$this->sortTimeBlocks();
-		foreach ( $this->getTimeBlocks() as $block ) {
-			$this->_addScheduledEvent(Event::create('BEGIN',$block->getBegin()));
-			$this->_addScheduledEvent(Event::create('END',$block->getEnd()));
-		}
-	}
-	
-	private function _addScheduledEvent($arg=NULL){
-		if ( $arg instanceof Event ) {
-			$this->_scheduled_events[] = $arg;
-		}
 	}
 }
 ?>
