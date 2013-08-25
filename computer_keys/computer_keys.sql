89860d6c27fd5274258aea529ae867344515ec4e
Mike Curtis <curtismi@upstate.edu>
2013-08-25 14:02:14 -0400

    Update computer_keys.sql

--- computer_keys/computer_keys.sql
+++ computer_keys/computer_keys.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2013 at 05:20 PM
+-- Generation Time: Aug 25, 2013 at 07:55 PM
 -- Server version: 5.6.11-log
 -- PHP Version: 5.4.14
 
@@ -35,7 +35,16 @@ CREATE TABLE IF NOT EXISTS `computers` (
   `note` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`,`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `computers`
+--
+
+INSERT INTO `computers` (`id`, `name`, `ip`, `active`, `timestamp`, `note`) VALUES
+(1, 'COMP-F7R6D4S3', '192.0.2.0', 'y', '2013-08-25 17:53:12', ''),
+(2, 'COMP-HH7T99KX', '192.0.2.1', 'y', '2013-08-25 17:53:33', ''),
+(3, 'COMP-88U4AQ2R', '192.0.2.2', 'y', '2013-08-25 17:53:53', '');
 
 -- --------------------------------------------------------
 
@@ -46,13 +55,13 @@ CREATE TABLE IF NOT EXISTS `computers` (
 CREATE TABLE IF NOT EXISTS `key_schedule` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
   `computer` int(3) NOT NULL,
-  `begin` datetime NOT NULL,
-  `end` datetime NOT NULL,
+  `begin` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `end` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `key` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
   `note` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=24 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
