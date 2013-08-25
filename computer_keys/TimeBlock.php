0eeece9676a2ecd3abb88f49dc1c91bfc75fcb4c
Mike Curtis <curtismi@upstate.edu>
2013-08-25 17:06:04 -0400

    Update TimeBlock.php

--- computer_keys/TimeBlock.php
+++ computer_keys/TimeBlock.php
@@ -4,15 +4,19 @@ class TimeBlock
 {
 	private $_begin = NULL;
 	private $_end = NULL;
+	private $_type = NULL;
+	private static $_type_scheduled = 'SCHEDULED';
+	private static $_type_available = 'AVAILABLE';
 	
-	private function __construct($begin=NULL,$end=NULL){
+	private function __construct($begin=NULL,$end=NULL,$type=NULL){
 		$this->_begin = $begin;
 		$this->_end = $end;
+		$this->_type = $type;
 	}
 	
-	public static function create($begin=NULL,$end=NULL){
-		if ( is_numeric($begin) && is_numeric($end) && ($begin < $end) ) {
-			return new TimeBlock($begin,$end);
+	public static function create($begin=NULL,$end=NULL,$type=NULL){
+		if ( is_numeric($begin) && is_numeric($end) && ($type === self::$_type_scheduled || $type === self::$_type_available) && ($begin < $end) ) {
+			return new TimeBlock($begin,$end,$type);
 		} else {
 			return FALSE;
 		}
@@ -26,8 +30,12 @@ class TimeBlock
 		return $this->_end;
 	}
 	
+	public function getType(){
+		return $this->_type;
+	}
+	
 	public function __toString(){
-		return date("M, j g:i:s a",$this->_begin).' to '.date("M, j g:i:s a",$this->_end);
+		return $this->_type.': '.date("M, j g:i:s a",$this->_begin).' to '.date("M, j g:i:s a",$this->_end);
     }
 }
 ?>
