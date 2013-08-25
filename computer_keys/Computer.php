e45b316fcf6e45331f439f48aa6bf82728c8007f
Mike Curtis <curtismi@upstate.edu>
2013-08-25 14:05:12 -0400

    Update Computer.php

--- computer_keys/Computer.php
+++ computer_keys/Computer.php
@@ -1,11 +1,15 @@
 <?php 
 
+require_once(dirname(__FILE__).'/Event.php');
+
 class Computer
 {
 	private $_name = NULL;
 	private $_ip = NULL;
 	private $_id = NULL;
 	private $_time_blocks = array();
+	private $_scheduled_events = array();
+	private $_available_events = array();
 	
 	private function __construct($name=NULL,$id=NULL,$ip=NULL){
 		$this->_name = $name;
@@ -41,6 +45,36 @@ class Computer
 	
 	public function getTimeBlocks(){
 		return $this->_time_blocks;
+	}
+	
+	public function getScheduledEvents(){
+		return $this->_scheduled_events;
+	}
+	
+	public function sortTimeBlocks(){
+		usort($this->_time_blocks,array('Computer','cmpTimeBlocks'));
+	}
+	
+	public static function cmpTimeBlocks($a,$b){
+		if ( $a instanceof TimeBlock && $b instanceof TimeBlock ) {
+			return ($a->getBegin() < $b->getBegin()) ? -1 : 1;
+		} else {
+			return 1;
+		}
+	}
+	
+	public function setScheduledEvents(){
+		$this->sortTimeBlocks();
+		foreach ( $this->getTimeBlocks() as $block ) {
+			$this->_addScheduledEvent(Event::create('BEGIN',$block->getBegin()));
+			$this->_addScheduledEvent(Event::create('END',$block->getEnd()));
+		}
+	}
+	
+	private function _addScheduledEvent($arg=NULL){
+		if ( $arg instanceof Event ) {
+			$this->_scheduled_events[] = $arg;
+		}
 	}
 }
 ?>
