27aabe5642e40af2200525b66255d896db975960
Mike Curtis <curtismi@upstate.edu>
2013-08-26 02:01:15 -0400

    Update TimeBlock.php

--- computer_keys/TimeBlock.php
+++ computer_keys/TimeBlock.php
@@ -5,18 +5,22 @@ class TimeBlock
 	private $_begin = NULL;
 	private $_end = NULL;
 	private $_type = NULL;
+	private $_id = NULL;
+	private $_key = NULL;
 	private static $_type_scheduled = 'SCHEDULED';
 	private static $_type_available = 'AVAILABLE';
 	
-	private function __construct($begin=NULL,$end=NULL,$type=NULL){
+	private function __construct($begin=NULL,$end=NULL,$type=NULL,$id=NULL,$key=NULL){
 		$this->_begin = $begin;
 		$this->_end = $end;
 		$this->_type = $type;
+		$this->_id = $id;
+		$this->_key = $key;
 	}
 	
-	public static function create($begin=NULL,$end=NULL,$type=NULL){
-		if ( is_numeric($begin) && is_numeric($end) && ($type === self::$_type_scheduled || $type === self::$_type_available) && ($begin < $end) ) {
-			return new TimeBlock($begin,$end,$type);
+	public static function create($begin=NULL,$end=NULL,$type=NULL,$id=NULL,$key=NULL){
+		if ( is_numeric($begin) && is_numeric($end) && ($type === self::$_type_scheduled || $type === self::$_type_available) && ($begin < $end) && is_numeric($id) && is_string($key) ) {
+			return new TimeBlock($begin,$end,$type,$id,$key);
 		} else {
 			return FALSE;
 		}
@@ -32,6 +36,14 @@ class TimeBlock
 	
 	public function getType(){
 		return $this->_type;
+	}
+	
+	public function getId(){
+		return $this->_id;
+	}
+	
+	public function getKey(){
+		return $this->_key;
 	}
 	
 	public function __toString(){
