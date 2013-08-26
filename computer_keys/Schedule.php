b7db22fbde96d98a5bf11c987877c706c710445a
Mike Curtis <curtismi@upstate.edu>
2013-08-25 23:30:05 -0400

    Update Schedule.php

--- computer_keys/Schedule.php
+++ computer_keys/Schedule.php
@@ -2,6 +2,14 @@
 
 class Schedule
 {
+	private $_day_begin = NULL;
+	private $_day_end = NULL;
+	private $_time_blocks = array();
+	private $_available_time_blocks = array();
 
+	public function __construct(){
+		$this->_day_begin = (string) strtotime(date("Y-m-d").' 00:00:00');
+		$this->_day_end = (string) strtotime(date("Y-m-d").' 23:59:59');
+  }
 }
 ?>
