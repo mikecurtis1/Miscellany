59e86339b188cc9661e05bedaf21c2043ec80676
Mike Curtis <curtismi@upstate.edu>
2013-08-27 19:18:34 -0400

    Update DbQuery.php

--- classes/DbQuery.php
+++ classes/DbQuery.php
@@ -1,7 +1,8 @@
 <?php 
+
 class DbQuery
 {
-	private $_count = 0;
+	private $_count = NULL;
 	private $_results = array();
 	private $_fields = array();
 	private $_affected_rows = NULL;
@@ -23,10 +24,14 @@ class DbQuery
 		}
 	}
 	
-	static public function query($conn=NULL,$db=NULL,$sql=NULL){
-		mysql_select_db($db,$conn);
-		if ( $results = mysql_query($sql,$conn) ) {
-			return new DbQuery($results);
+	static public function query($link=NULL,$db_name=NULL,$sql=NULL,$type='mysql'){
+		if ( is_resource($link) && is_string($db_name) ) {
+			mysql_select_db($db_name,$link);
+		} else {
+			return FALSE;
+		}
+		if ( is_string($sql) && is_resource($link)  ) {
+			return new DbQuery(mysql_query($sql,$link));
 		} else {
 			return FALSE;
 		}
@@ -67,35 +72,6 @@ class DbQuery
 		} else {
 			return 'odd';
 		}
-	}
-	
-	public function htmlFieldsForm($mode=NULL){
-		ob_start();
-		foreach ( $this->_fields as $key => $field ) {
-			if ( $mode === 'update' && isset($this->_results[0]) ) {
-				$value = htmlspecialchars($this->_results[0][$field->name]);
-			} else {
-				$value = '';
-			}
-			if ( $mode === 'update' && ($field->primary_key === 1 || $field->type === 'timestamp') ) {
-				echo '<div><label>'.$field->name.'</label><input class="disabled" type="text" disabled="disabled" name="'.$field->name.'" value="'.$value.'" /></div>'."\n";
-				if ( $field->primary_key === 1 ) {
-					echo '<input type="hidden" name="'.$field->name.'" value="'.$value.'" />'."\n";
-				}
-			} elseif ( $field->primary_key !== 1 && $field->type !== 'timestamp' ) {
-				if ( $field->numeric === 1 ) {
-					echo '<div><label>'.$field->name.'</label><input class="text" type="text" name="'.$field->name.'" value="'.$value.'" /></div>'."\n";
-				} elseif ( $field->numeric === 0 ) {
-					echo '<div><label>'.$field->name.'</label><textarea name="'.$field->name.'">'.$value.'</textarea></div>'."\n";
-				} else {
-					echo "\n";
-				}
-			}
-		}
-		$contents = ob_get_contents();
-		ob_end_clean();
-		
-		return $contents;
 	}
 	
 	public function htmlResultsTable($link_key=NULL,$link_base=NULL){
