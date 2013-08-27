4ef5e9700de76b9fd6941910f65f2e6826cefa78
Mike Curtis <curtismi@upstate.edu>
2013-08-27 19:21:38 -0400

    Update Computers.php

--- computer_keys/Computers.php
+++ computer_keys/Computers.php
@@ -1,23 +1,18 @@
 <?php 
 
-require_once(dirname(__FILE__).'/../classes/DbQuery.php');
-require_once(dirname(__FILE__).'/../classes/DbConnection.php');
-require_once(dirname(__FILE__).'/Computer.php');
-
 class Computers
 {
-	private $_db_name = 'computer_keys';
-	private $_db_connection = FALSE;
+	private $_dbc = NULL;
 	private $_computers = array();
 	
-	private function __construct($c=NULL){
-		$this->_db_connection = $c->getConnection();
+	private function __construct($arg=NULL){
+		$this->_dbc = $arg;
 		$this->_setComputers();
 	}
 	
-	public static function create($db_host=NULL,$db_username=NULL,$db_password=NULL){
-		if ( $c = DbConnection::create($db_host,$db_username,$db_password) ) {
-			return new Computers($c);
+	public static function create($arg=NULL){
+		if ( $arg instanceof DbConnection ) {
+			return new Computers($arg);
 		} else {
 			return FALSE;
 		}
@@ -25,9 +20,10 @@ class Computers
 	
 	private function _setComputers(){
 		$sql = 'SELECT * FROM computers WHERE active = \'y\'';
-		if ( $query = DbQuery::query($this->_db_connection,$this->_db_name,$sql) ) {
+		if ( $query = DbQuery::query($this->_dbc->getLink(),$this->_dbc->getDbName(),$sql) ) {
 			foreach ( $query->getResults() as $row ) {
 				if ( $c = Computer::create($row['name'],$row['id'],$row['ip']) ) {
+					$c->setSchedule($this->_dbc);
 					$this->_computers[$row['name']] = $c;
 				}
 			}
