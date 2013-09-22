79817240de02149af172b7cd60e38cba7f80e9dc
Mike Curtis <curtismi@upstate.edu>
2013-09-22 16:44:12 -0400

    Update TagReader.php

--- ontology/TagReader.php
+++ ontology/TagReader.php
@@ -1,58 +1,3 @@
-<pre>
-<?php 
-$exceptions = array();
-$path_delimiter = '::';
-$tag_sets[] = 'beef,animal,food';
-$tag_sets[] = 'animal,fish,food';
-$tag_sets[] = 'vegetable,lettuce,leaf,food';
-$tag_sets[] = 'spinach,leaf,food,vegetable';
-$tag_sets[] = 'food,legume,vegetable,peas';
-$tag_sets[] = 'burger,grub,cow';
-$tag_sets[] = 'burger,beef,food';
-$tag_synonyms = array('grub'=>'food','cow'=>'beef');
-#$tag_sets = 'kittens'; // force an exception for testing
-#$tag_sets = array('pet'=>FALSE); // testing
-#$tag_sets = array('pet'=>'kittens'); // testing
-#$tag_sets = array('kittens'); // testing
-try {
-	$t = new TagReader($path_delimiter,$tag_sets,$tag_synonyms);
-} catch (Exception $e ) {
-	$exceptions[] = $e->getMessage();
-}
-if ( ! empty($exceptions) ) {
-	print_r($exceptions);
-	die('Exceptions must be resolved before the program can continue running.');
-}
-echo "\n------------------------------------------------------------\n";
-echo "INPUT tag sets:\n";
-print_r($tag_sets);
-echo "\n------------------------------------------------------------\n";
-echo "INPUT tag synonyms:\n";
-print_r($tag_synonyms);
-echo "\n------------------------------------------------------------\n";
-echo "getTagSets:\n\n";
-print_r($t->getTagSets());
-echo "\n------------------------------------------------------------\n";
-echo "getTagSynonyms:\n\n";
-print_r($t->getTagSynonyms());
-echo "\n------------------------------------------------------------\n";
-echo "getTagList:\n\n";
-print_r($t->getTagList());
-echo "\n------------------------------------------------------------\n";
-echo "getTagCounts:\n\n";
-print_r($t->getTagCounts());
-echo "\n------------------------------------------------------------\n";
-echo "getTagIndex:\n\n";
-print_r($t->getTagIndex());
-echo "\n------------------------------------------------------------\n";
-echo "getTagRanks:\n\n";
-print_r($t->getTagRanks());
-echo "\n------------------------------------------------------------\n";
-echo "getTagPaths:\n\n";
-print_r($t->getTagPaths());
-echo "\n------------------------------------------------------------\n";
-?>
-</pre>
 <?php 
 
 class TagReader
@@ -65,18 +10,18 @@ class TagReader
 	private $_tag_index = array();
 	private $_tag_ranks = array();
 	private $_tag_paths = array();
+	private $_rank_limit = NULL;
+	private $_uncategorized_label = NULL;
 	private $_tag_synonyms = array('grub'=>'food','cow'=>'beef');
 	
-	public function __construct($path_delimiter=NULL,$tag_sets=array(),$tag_synonyms=array()){
+	public function __construct($path_delimiter='::',$tag_sets=array(),$tag_synonyms=array(),$rank_limit=0,$uncategorized_label='Miscellaneous'){
 		if ( is_string($path_delimiter) ) {
 			$this->_path_delimiter = $path_delimiter;
-		} else {
-			throw new Exception('Error: path delimiter must be a string.');
 		}
 		if ( is_array($tag_sets) ) { 
 			foreach ( $tag_sets as $k => $v ) {
 				if ( is_string($v) ) {
-					$this->_tag_sets[] = $v;
+					$this->_tag_sets[$k] = $v;
 				}
 			}
 		} else {
@@ -90,6 +35,12 @@ class TagReader
 			}
 		} else {
 			throw new Exception('Error: tag_synonyms must be an array.');
+		}
+		if ( is_int($rank_limit) ) {
+			$this->_rank_limit = $rank_limit;
+		}
+		if ( is_string($uncategorized_label) ) {
+			$this->_uncategorized_label = $uncategorized_label;
 		}
 		$this->_setTagList();
 		$this->_setTagCounts();
@@ -146,17 +97,24 @@ class TagReader
 	}
 	
 	private function _setTagPaths(){
-		foreach ( $this->_tag_sets as $set ) {
+		foreach ( $this->_tag_sets as $k =>$set ) {
 			$temp = array();
 			foreach ( explode($this->_tag_delimiter,$set) as $tag ) {
 				if ( isset($this->_tag_synonyms[$tag]) ) {
 					$temp[$this->_tag_synonyms[$tag].'('.$tag.')'] = $this->_tag_ranks[$this->_tag_synonyms[$tag]];
 				} else {
-					$temp[$tag] = $this->_tag_ranks[$tag];
+					if ( $this->_tag_ranks[$tag] <= $this->_rank_limit ) {
+						$temp[$tag] = $this->_tag_ranks[$tag];
+					}
 				}
 			}
-			asort($temp);
-			$this->_tag_paths[] = implode($this->_path_delimiter,array_keys($temp));
+			if ( !empty($temp) ) {
+				asort($temp);
+				$this->_tag_paths[$k] = implode($this->_path_delimiter,array_keys($temp));
+			} else {
+				$this->_tag_paths[$k] = $this->_uncategorized_label;
+			}
+			
 		}
 	}
 	
