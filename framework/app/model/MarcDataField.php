d626c479bf4aa4e5f72f68903eb35f16b666018f
Mike Curtis <curtismi@upstate.edu>
2014-11-03 13:41:13 -0500

    Update MarcDataField.php

--- framework/app/model/MarcDataField.php
+++ framework/app/model/MarcDataField.php
@@ -6,15 +6,17 @@ class MarcDataField
 	private $_identifier1 = NULL;
 	private $_identifier2 = NULL;
 	private $_subfields = array();
+	private $_field_terminator = '';
 	
 	private function __construct($tag=NULL,$i1=NULL,$i2=NULL){
 		$this->_tag = $tag;
 		$this->_identifier1 = $i1;
 		$this->_identifier2 = $i2;
+		$this->_field_terminator = chr(30); // record separator
 	}
 
 	static public function build($tag=NULL,$i1=NULL,$i2=NULL){
-		if ( ( is_numeric($tag) && strlen($tag) === 3 ) && ( is_string($i1) && strlen($i1) === 1 ) && ( is_string($i2) && strlen($i2) === 1 ) ) {	
+		if ( ( is_string($tag) && strlen($tag) === 3 ) && ( is_string($i1) && strlen($i1) === 1 ) && ( is_string($i2) && strlen($i2) === 1 ) ) {	
 			return new MarcDataField($tag,$i1,$i2);
 		} else {
 			return FALSE;
@@ -41,6 +43,14 @@ class MarcDataField
 	
 	public function getSubfields(){
 		return $this->_subfields;
+	}
+	
+	public function getDataField($arg=FALSE){
+		if ( is_bool($arg) && $arg === TRUE ) {
+			return $this->_identifier1 . $this->_identifier2 . trim($this->_subfields) . $this->_field_terminator;
+		} else {
+			return $this->_identifier1 . $this->_identifier2 . trim($this->_subfields);
+		}
 	}
 }
 ?>
