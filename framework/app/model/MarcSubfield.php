a87629a64fe4937c4febedcf06a6d0b65b7c9a03
Mike Curtis <curtismi@upstate.edu>
2014-11-03 13:41:41 -0500

    Update MarcSubfield.php

--- framework/app/model/MarcSubfield.php
+++ framework/app/model/MarcSubfield.php
@@ -4,15 +4,19 @@ class MarcSubfield
 {
 	private $_code = NULL;
 	private $_data = NULL;
+	private $_subfield_indicator = ''; // prefix to subfield codes
+	private $_subfield_indicator_graphic = '';
 	
-	private function __construct($code=NULL,$data=NULL){
+	private function __construct($code=NULL,$data=NULL,$subfield_indicator_graphic=''){
 		$this->_code = $code;
 		$this->_data = $data;
+		$this->_subfield_indicator = chr(31); // unit separator
+		$this->_subfield_indicator_graphic = $subfield_indicator_graphic;
 	}
 
-	static public function build($code=NULL,$data=NULL){
-		if ( ( is_string($code) && strlen($code) === 1 ) && is_string($data) ) {	
-			return new MarcSubfield($code,$data);
+	static public function build($code=NULL,$data=NULL,$subfield_indicator_graphic='$'){
+		if ( ( is_string($code) && strlen($code) === 1 ) && is_string($data) && ( is_string($subfield_indicator_graphic) && strlen($subfield_indicator_graphic) === 1 ) ) {	
+			return new MarcSubfield($code,$data,$subfield_indicator_graphic);
 		} else {
 			return FALSE;
 	 	}
@@ -24,6 +28,15 @@ class MarcSubfield
 	
 	public function getData(){
 		return $this->_data;
+	}
+	
+	public function getSubfieldStr($arg=FALSE){
+		if ( $arg === TRUE ) {
+			$d = str_replace($this->_subfield_indicator_graphic, '\\' . $this->_subfield_indicator_graphic, trim($this->_data));
+			return trim($this->_code) . $this->_subfield_indicator_graphic . $d;
+		} else {
+			return trim($this->_code) . $this->_subfield_indicator . trim($this->_data);
+		}
 	}
 }
 ?>
