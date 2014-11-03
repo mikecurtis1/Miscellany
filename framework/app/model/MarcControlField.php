b8c019eec776959458e79b4e9405d7a5652398b0
Mike Curtis <curtismi@upstate.edu>
2014-11-03 13:40:21 -0500

    Update MarcControlField.php

--- framework/app/model/MarcControlField.php
+++ framework/app/model/MarcControlField.php
@@ -5,10 +5,12 @@ class MarcControlField
 	private $_tag = NULL;
 	private $_data = NULL;
 	static private $_control_field_tags = array('FMT','001','003','005','006','007','008','009');
+	private $_field_terminator = '';
 	
 	private function __construct($tag=NULL,$data=NULL){
 		$this->_tag = $tag;
 		$this->_data = $data;
+		$this->_field_terminator = chr(30); // record separator
 	}
 
 	static public function build($tag=NULL,$data=NULL){
@@ -25,6 +27,14 @@ class MarcControlField
 	
 	public function getData(){
 		return $this->_data;
+	}
+	
+	public function getControlField($arg=FALSE){
+		if ( is_bool($arg) && $arg === TRUE ) {
+			return trim($this->_data) . $this->_field_terminator;
+		} else {
+			return trim($this->_data);
+		}
 	}
 }
 ?>
