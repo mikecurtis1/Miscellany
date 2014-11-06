46ebee0d5a3337d596edbca4680d13e1ec87e510
Mike Curtis <curtismi@upstate.edu>
2014-11-06 13:35:05 -0500

    Update Interval.php

--- ToneSet/Interval.php
+++ ToneSet/Interval.php
@@ -4,12 +4,12 @@ abstract class Interval
 {
 	protected $_tone_set = array();
 	
-	protected function _getToneByIntervalAsc($str,$interval){
-		$ordinal = MusicTables::getASPNValue($str,'key') + MusicTables::getIntervalValue($interval,'chromatic_steps');
-		$letter = MusicTables::getNextLetter($str,$interval);
-		$aspn = MusicTables::getPianoKeySpelling($ordinal,$letter);
+	protected function _getToneByIntervalAsc($aspn, $interval, $tables){
+		$temp_piano_key = $tables::getASPNValue($aspn, 'piano_key') + $tables::getIntervalValue($interval, 'chromatic_steps');
+		$temp_letter = $tables::getNextLetter($aspn, $interval);
+		$temp_aspn = $tables::getPianoKeySpelling($temp_piano_key, $temp_letter);
 		try {
-			$tone = Tone::create($aspn);
+			$tone = Tone::create($temp_aspn, $tables);
 		} catch (Exception $e) {
 			throw new Exception($e->getMessage());
 		}
@@ -17,20 +17,8 @@ abstract class Interval
 		return $tone;
 	}
 	
-	public function addTones($aspn='C4',$interval='P1'){
-		if ( ! MusicTables::isASPN($aspn) ) {
-			throw new Exception($aspn . ' is NOT an ASPN value.');
-		}
-		if ( ! MusicTables::isInterval($interval) ) {
-			throw new Exception($interval . ' is NOT an interval abbreviation.');
-		}
-		$this->_tone_set[0] = $this->_getToneByIntervalAsc($aspn,$interval);
-	}
-	
-	public function getToneSet(){
-		return $this->_tone_set;
-	}
-	
+	public function addTones($aspn, $interval, MusicTables $tables){}
+	public function getToneSet(){}
 	public function permute(){}
 	public function retrograde(){}
 	public function truncate(){}
