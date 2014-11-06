a224bf5a32597f9fc20221310bee11b0b056d1c9
Mike Curtis <curtismi@upstate.edu>
2014-11-06 14:08:57 -0500

    Create singleton_factory_DI.php

---
+++ oop/samples/singleton_factory_DI.php
@@ -0,0 +1,79 @@
+<?php 
+
+class DataTable
+{
+	// singleton pattern
+	
+	private static $_data = array();
+	
+	protected function __construct(){}
+	private function __clone(){}
+	private function __wakeup(){}
+	
+	public static function config($json_path=''){
+		self::$_data = json_decode(file_get_contents($json_path),TRUE);
+		static $instance = null;
+		if ( NULL === $instance ) {
+			$instance = new static();
+		}
+		return $instance;
+	}
+	
+	public static function getDataValue($key){
+		if ( isset(self::$_data[$key]) ) {
+			return self::$_data[$key];
+		} else {
+			return FALSE;
+		}
+	}
+	
+	public static function isValidKey($key){
+		if ( isset(self::$_data[$key]) ) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+}
+
+class KEV
+{
+	// factory pattern, type-hinted dependency injection
+	
+	private $_key = '';
+	private $_value = '';
+	
+	private function __construct($key,$table){
+		$this->_key = $key;
+		$this->_value = $table::getDataValue($key);
+	}
+
+	public static function create($key='',DataTable $table){
+		if ( $table::isValidKey($key) ) {
+			return new KEV($key,$table);
+		} else {
+			return FALSE;
+		}
+	}
+	
+	public function getKey(){
+		return $this->_key;
+	}
+	public function getValue(){
+		return $this->_value;
+	}
+}
+?>
+<pre>
+<?php 
+
+$table = DataTable::config('file.json');
+echo var_dump($table);
+echo var_dump(KEV::create('D',$table));
+echo var_dump(KEV::create('3',$table));
+echo var_dump(KEV::create('foobar',$table));
+
+$table2 = DataTable::config('file.json'); // does NOT return a second instance
+echo var_dump($table2);
+?>
+</pre>
