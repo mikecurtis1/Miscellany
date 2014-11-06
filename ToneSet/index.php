8ed7cdd5633ddd7ff72d6b43c1c4f35f35be597b
Mike Curtis <curtismi@upstate.edu>
2014-11-06 17:19:12 -0500

    Update index.php

--- ToneSet/index.php
+++ ToneSet/index.php
@@ -3,17 +3,27 @@
 
 $tables = MusicTables::config();
 
+echo "<hr />\n";
 echo var_dump($tables::getASPNValue('C4', 'piano_key'));
 echo "\n";
-echo "<hr />\n";
 
+echo "<hr />\n";
 $chord = new Chord('C4', 'minor', $tables);
 foreach ( $chord->getToneSet() as $tone ) {
 	echo $tone->getASPN() . ' ';
 }
-echo "\n";
-echo "<hr />\n";
+echo "\n\n";
 echo var_dump($chord);
+echo "\n";
+
+echo "<hr />\n";
+$scale = new Scale('C4', 'harmonic_minor', $tables);
+foreach ( $scale->getToneSet() as $tone ) {
+	echo $tone->getASPN() . ' ';
+}
+echo "\n\n";
+echo var_dump($scale);
+echo "\n";
 
 ?>
 </pre>
